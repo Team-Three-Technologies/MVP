f3957e95ee19a053a14099d281807e830c2f7e77
Filippo Compagno <filippo.compag@gmail.com>
2026-04-02 11:37:43 +0200

    iniziato mapping

--- main/src/mappers/dip.mapper.ts
+++ main/src/mappers/dip.mapper.ts
@@ -1,3 +1,13 @@
+import { DiPIndexXml } from '../infrastructure/parsing/dip-index.xml';
+import { Dip } from '../domain/dip.model';
+
 export class DipMapper {
-  
+  toDomain(dipIndexXml: DiPIndexXml): Dip {
+    return new Dip(
+      dipIndexXml.DiPIndex.PackageInfo.ProcessUUID,
+      new Date(dipIndexXml.DiPIndex.PackageInfo.CreationDate),
+      Number(dipIndexXml.DiPIndex.PackageInfo.DocumentsCount),
+      Number(dipIndexXml.DiPIndex.PackageInfo.AiPCount)
+    );
+  }
 }
--- main/src/mappers/document.mapper.ts
+++ main/src/mappers/document.mapper.ts
@@ -1,2 +1,10 @@
+import { DocumentMetadataXml } from '../infrastructure/parsing/document-metadata.xml';
+import { Document } from '../domain/document.model';
+import { File } from '../domain/file.model';
 export class DocumentMapper {
+  toDomain(documentMetadata: DocumentMetadataXml): Document {
+    return new Document(
+      documentMetadata.Document.DocumentoInformatico.IdDoc.Identificativo,
+    );
+  }
 }
