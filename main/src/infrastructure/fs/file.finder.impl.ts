08c7c9c2443053c4752b11b7bd93744f2d27f983
Filippo Compagno <filippo.compag@gmail.com>
2026-04-13 23:19:26 +0200

    piccolo fix di dipendenze

--- main/src/infrastructure/fs/file.finder.impl.ts
+++ main/src/infrastructure/fs/file.finder.impl.ts
@@ -1,12 +1,24 @@
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
+import { TOKENS } from '../di/tokens';
 import { FileFinder } from './file.finder.interface';
+import { AppConfig } from '../app.config';
 import * as fsp from 'node:fs/promises';
 import * as path from 'node:path';
 
 @injectable()
 export class FileFinderImpl implements FileFinder {
+  constructor(
+    @inject(TOKENS.AppConfig)
+    private readonly config: AppConfig
+  ) { }
+
   private readonly DIP_INDEX_NAME_PATTERN = /^DiPIndex\.\d{4}\d{1,2}\d{1,2}\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.xml$/i; // DiPIndex.AAAA[M o MM][G o GG].UUID.xml
   private readonly AIP_INFO_NAME_PATTERN = /^AiPInfo\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\.xml$/i;
+
+  public getStartDir(): string {
+    return this.config.appDir;
+  }
+
 
   public async findDipIndex(dir: string): Promise<string | null> {
     const files = await fsp.readdir(dir);
