6a75bd927814acb8fb7e60881a2111408b703221
Andrea Masiero <andrea.masiero.15@studenti.unipd.it>
2026-04-14 11:58:59 +0200

    aggiornamento

--- renderer/src/main.ts
+++ renderer/src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { setupElectronApiMock } from './app/mocks/electron-api.mock';
-
-// Setup mock for local development and testing purposes
-setupElectronApiMock();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
