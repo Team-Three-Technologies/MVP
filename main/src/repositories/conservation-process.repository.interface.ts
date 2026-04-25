3b3a7f9e2edf2fd01ec351b4f40f778b10d97eea
nenadkk <nnrdlvc@gmail.com>
2026-04-25 16:57:50 +0200

    Scritti test di unità dip.repository e conservation-process.repository

--- main/src/repositories/conservation-process.repository.interface.ts
+++ main/src/repositories/conservation-process.repository.interface.ts
@@ -2,9 +2,4 @@ import { ConservationProcess } from '../domain/conservation-process.model';
 
 export interface ConservationProcessRepository {
   save(conservationProcess: ConservationProcess): Promise<ConservationProcess>;
-  findAllByDipUuid(dipUuid: string): Promise<ConservationProcess[]>;
-  findAllByDocumentClassUuidAndVersion(
-    documentClassUuid: string,
-    documentClassVersion: string,
-  ): Promise<ConservationProcess[]>;
 }
---
+++ main/test/repositories/conservation-process.repository.test.ts
@@ -0,0 +1,64 @@
+import { container } from 'tsyringe';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { TOKENS } from '../../src/infrastructure/di/tokens';
+
+import { SQLiteConservationProcessRepository } from '../../src/repositories/conservation-process.repository.sqlite';
+import { ConservationProcess } from '../../src/domain/conservation-process.model';
+
+describe('Test SQLiteDocumentClassRepository', ()=>{
+    beforeEach(()=>{
+        container.clearInstances();
+        container.register(TOKENS.DatabaseProvider, { useValue: {} });
+    });
+
+    it('save(conservationProcess), se avviene con successo restituisce il conservationProcess passato', async ()=>{
+
+        const mockContainer ={
+            prepare : vi.fn().mockReturnValue({
+                run:vi.fn()}),
+        };
+        const mockDatabaseProvider={
+            instance: mockContainer,
+        };
+        container.register(TOKENS.DatabaseProvider, {
+            useValue: mockDatabaseProvider,
+        });
+
+        const procConsRepo = container.resolve(SQLiteConservationProcessRepository);
+
+        const result = await procConsRepo
+            .save(new ConservationProcess(
+                'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                new Date(),
+                '10 byte',
+                10,
+                10,
+                10,
+                'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+                'v1.0'));
+        expect(result).toBeInstanceOf(ConservationProcess);
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
